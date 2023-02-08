0b9277df53f2bc553cf754020cdd815db2add838
Apkunpacker <apkunpacker@gmail.com>
2023-02-08 21:51:25 +0530

    Update UltraFinder.js

--- Windows/UltraFinder.js
+++ Windows/UltraFinder.js
@@ -1,21 +1,12 @@
-setTimeout(function() { 
-        var UF = Process.findModuleByName("ProtectionPlusDLL.dll");
-        Module.enumerateExportsSync(UF.name).forEach(function (exp) {
-         /*We can also use single export name but this way is mor easy if we have to deal with more exports . Remove // from console.log lines if want to see input perameter and return value*/   
-       if ((exp.name).indexOf('IsSubscriptionLicense') || (exp.name).indexOf("IsInitializedSoftwareKey")!= -1) {
-          try {
-               Interceptor.attach(ptr(exp.address), {
-                     onEnter: function (args) {
-                           //console.log(exp.name + " argument : "+ args[0]);
-                        },
-                      onLeave: function(retval)  {         
-                             //console.warn(exp.name + " [ Retval ] : "+retval);
-                            // console.warn("We are registered now");
-							 retval.replace(1);
-                             
-                     }
-            });
-        } catch (e) { } 
+setTimeout(function() {
+    Interceptor.attach(Module.findExportByName("ProtectionPlusDLL.dll", "IsSubscriptionLicense"), {
+        onLeave: function(retval) {
+            retval.replace(1);
         }
-    });
-    }, 1000);
+    })
+    Interceptor.attach(Module.findExportByName("ProtectionPlusDLL.dll", "IsInitializedSoftwareKey"), {
+        onLeave: function(retval) {
+            retval.replace(1);
+        }
+    })
+}, 1000);
